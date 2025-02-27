f003fceb35005340606b05390b963f51ff333270
sSsamirRr-mine <sssamirrr4@gmail.com>
2025-02-27 23:37:47 +0500

    Add files via upload

---
+++ Hayvonlar.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import { data } from "./Hayvonlar_Storage.js";
+let main = document.querySelector("section")
+let input = document.querySelector("input")
+function CreateCard(from) {
+    let card = document.createElement("div")
+    let images = document.createElement("img")
+    let name = document.createElement("h2")
+    let cardInfo = document.createElement("p")
+    card.classList.add("card")
+    images.classList.add("cardImg")
+    name.classList.add("title")
+    cardInfo.classList.add("cardInfo")
+    images.src = from.rasmi
+    name.innerHTML = from.nomi
+    cardInfo.innerHTML = from.tavsif
+    card.appendChild(images)
+    card.appendChild(name)
+    card.appendChild(cardInfo)
+    main.appendChild(card)
+}
+data.forEach(item => {
+    CreateCard(item)
+});
+input.addEventListener("input", () => {
+    let redact = data.filter((Item) =>
+        Item.title.toLowerCase().trim().includes(input.value.toLowerCase().trim())
+    )
+    main.innerHTML = ""
+    redact.forEach(Item => {
+        CreateCard(Item)
+    });
+})
